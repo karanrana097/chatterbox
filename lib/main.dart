1755d6b1869f68d1dfd402554a74e2ae44464acb
KARAN RANA <karanrana1706@gmail.com>
2023-08-01 16:50:27 +0530

    Hero animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
-// import 'screens/login_screen.dart';
-// import 'screens/registration_screen.dart';
-// import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/chat_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -10,12 +10,20 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           bodyMedium: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      //home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id : (context)=> WelcomeScreen(),
+        LoginScreen.id : (context)=> LoginScreen(),
+        RegistrationScreen.id : (context)=> RegistrationScreen(),
+        ChatScreen.id : (context)=> ChatScreen(),
+      },
     );
   }
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,15 +1,42 @@
+import 'package:chatterbox/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
+//import 'registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
+
+  static const String id = 'welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = AnimationController(vsync: this, duration:  Duration(seconds: 1));
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white).animate((controller));
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    },);
+  }
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -18,12 +45,15 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60,
+                  ),
                 ),
                 Text(
-                  'Flash Chat',
+                  'ChatterBox',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
@@ -43,6 +73,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -61,6 +92,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
